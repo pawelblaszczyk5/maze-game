4c355e054be106cedc1fbc36c1facce31f162e0b
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-11 21:33:31 +0100

    Add player position

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,34 +1,32 @@
-import {Maze, MazeCell} from '../../helpers/maze/maze';
+import {CellCoordinates, Maze, MazeCell} from '../../helpers/maze/maze';
 import './Board.css';
 
-export const Board = ({maze}: { maze: Maze }) => {
-
-    const getBorders = (cell: MazeCell) => {
-      return {
-        borderBottomWidth: cell.bottomWall ? '1px' : undefined,
-        borderLeftWidth: cell.leftWall ? '1px' : undefined,
-        borderTopWidth: cell.topWall ? '1px' : undefined,
-        borderRightWidth: cell.rightWall ? '1px' : undefined
-      };
+export const Board = ({maze, player}: { maze: Maze, player: CellCoordinates }) => {
+  const getCellStyle = (cell: MazeCell) => {
+    return {
+      borderBottomWidth: cell.bottomWall ? '1px' : undefined,
+      borderLeftWidth: cell.leftWall ? '1px' : undefined,
+      borderTopWidth: cell.topWall ? '1px' : undefined,
+      borderRightWidth: cell.rightWall ? '1px' : undefined,
+      backgroundColor: (cell.coordinates.x === player.x && cell.coordinates.y === player.y) ? 'yellow' : undefined
     };
+  };
 
-    const getGridStyles = () => {
-      return {
-        gridTemplateColumns: `repeat(${maze[0].length},1fr)`,
-        gridTemplateRows: `repeat(${maze.length},1fr)`
-      };
+  const getGridStyles = () => {
+    return {
+      gridTemplateColumns: `repeat(${maze[0].length},1fr)`,
+      gridTemplateRows: `repeat(${maze.length},1fr)`
     };
+  };
 
-    return (
-      <div className="Board" style={getGridStyles()}>
-        {maze && maze.map((row) => {
-          return row.map((cell) => (
-            <div style={getBorders(cell)} key={`${cell.coordinates.y}${cell.coordinates.x}`} className="BoardCell">
+  return (
+    <div className="Board" style={getGridStyles()}>
+      {maze && maze.map((row) => {
+        return row.map((cell) => (
+          <div style={getCellStyle(cell)} key={`${cell.coordinates.y}${cell.coordinates.x}`} className="BoardCell"/>
+        ));
+      })}
+    </div>
+  );
+};
 
-            </div>
-          ));
-        })}
-      </div>
-    );
-  }
-;
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -11,14 +11,15 @@ export const Game = () => {
   const [maze, setMaze] = useState<Maze>();
   const [width, setWidth] = useState<number>(30);
   const [height, setHeight] = useState<number>(30);
-  
+  const [playerPosition, setPlayerPosition] = useState<CellCoordinates>({x: 0, y: 0});
+
   useEffect(() => {
     setMaze(generateMaze(width, height, getRandomCell(width, height)));
   }, [width, height]);
 
   return (
     <div className="Game">
-      {maze && <Board maze={maze}/>}
+      {maze && <Board maze={maze} player={playerPosition}/>}
     </div>
   );
 };
