b5517e50d963da50e445a073d039b6dcc751f017
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-13 08:09:33 +0100

    Setup reducer to handle player movement

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,17 +1,31 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useReducer, useState} from 'react';
 import {CellCoordinates, generateMaze, Maze} from '../../helpers/maze/maze';
 import {Board} from '../Board/Board';
+import useEventListener from '@use-it/event-listener';
 
 const getRandomCell = (width: number, height: number): CellCoordinates => ({
   x: Math.floor(Math.random() * (width + 1)),
   y: Math.floor(Math.random() * (height + 1)),
 });
 
+const arrowKeys = ['ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowRight'];
+
+const playerPositionReducer = (state: CellCoordinates, key: KeyboardEvent): CellCoordinates => {
+  console.log(state, key);
+  return {x: 0, y: 0};
+};
+
 export const Game = () => {
   const [maze, setMaze] = useState<Maze>();
   const [width, setWidth] = useState<number>(30);
   const [height, setHeight] = useState<number>(30);
-  const [playerPosition, setPlayerPosition] = useState<CellCoordinates>({x: 0, y: 0});
+  const [playerPosition, setPlayerPosition] = useReducer(playerPositionReducer, {x: 0, y: 0});
+
+  const handler = (e: KeyboardEvent) => {
+    setPlayerPosition(e);
+  };
+
+  useEventListener('keydown', handler);
 
   useEffect(() => {
     setMaze(generateMaze(width, height, getRandomCell(width, height)));
