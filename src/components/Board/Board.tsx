e7173ca9286884772a53f77f08081052de1dca1f
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-20 09:22:54 +0100

    Added exit indicator

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,13 +2,21 @@ import {CellCoordinates, Maze, MazeCell} from '../../helpers/maze/maze';
 import './Board.css';
 
 export const Board = ({maze, player}: { maze: Maze, player: CellCoordinates }) => {
-  const getCellStyle = (cell: MazeCell) => {
+  const getCellStyle = (cell: MazeCell, maze: Maze) => {
+    let backgroundColor: string | undefined;
+    if (cell.coordinates.x === player.x && cell.coordinates.y === player.y) {
+      backgroundColor = '#8a97a0';
+    } else if (cell.coordinates.y === maze.length - 1 && cell.coordinates.x === maze[0].length - 1) {
+      backgroundColor = '#3e1f47';
+    } else {
+      backgroundColor = undefined;
+    }
     return {
       borderBottomWidth: cell.bottomWall ? '1px' : undefined,
       borderLeftWidth: cell.leftWall ? '1px' : undefined,
       borderTopWidth: cell.topWall ? '1px' : undefined,
       borderRightWidth: cell.rightWall ? '1px' : undefined,
-      backgroundColor: (cell.coordinates.x === player.x && cell.coordinates.y === player.y) ? '#8a97a0' : undefined
+      backgroundColor: backgroundColor
     };
   };
 
@@ -23,7 +31,8 @@ export const Board = ({maze, player}: { maze: Maze, player: CellCoordinates }) =
     <div className="Board" style={getGridStyles()}>
       {maze && maze.map((row) => {
         return row.map((cell) => (
-          <div style={getCellStyle(cell)} key={`${cell.coordinates.y}${cell.coordinates.x}`} className="BoardCell"/>
+          <div style={getCellStyle(cell, maze)} key={`${cell.coordinates.y}${cell.coordinates.x}`}
+               className="BoardCell"/>
         ));
       })}
     </div>
