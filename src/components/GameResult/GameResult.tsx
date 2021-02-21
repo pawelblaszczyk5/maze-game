a4941a93f0dbab926d2379df8c7cf71df108f675
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-21 12:58:05 +0100

    Differentiate game result messages based on scores

--- src/components/GameResult/GameResult.tsx
+++ src/components/GameResult/GameResult.tsx
@@ -1,23 +1,62 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTrophy} from '@fortawesome/free-solid-svg-icons';
 import './GameResult.css';
+import {useEffect, useState} from 'react';
 
 export const GameResult = ({moves, bestMoves}: { moves: number, bestMoves: number }) => {
-  let color: string;
-
-  if (moves * 0.8 <= bestMoves) {
-    color = '#4d194d';
-  } else if (moves * 0.6 <= bestMoves) {
-    color = '#3e1f47';
-  } else {
-    color = '312244';
+  enum GameScore {
+    GOOD,
+    MEDIOCRE,
+    BAD
   }
+
+  const [gameScore, setGameScore] = useState<GameScore>(GameScore.BAD);
+
+  useEffect(() => {
+    if (moves * 0.85 <= bestMoves) {
+      setGameScore(GameScore.GOOD);
+    } else if (moves * 0.7 <= bestMoves) {
+      setGameScore(GameScore.MEDIOCRE);
+    } else {
+      setGameScore(GameScore.BAD);
+    }
+  }, [GameScore, moves, bestMoves]);
+
+  const getColorByScore = (score: GameScore): string => {
+    switch (score) {
+      case GameScore.GOOD: {
+        return '#4d194d';
+      }
+      case GameScore.MEDIOCRE: {
+        return '#3e1f47';
+      }
+      case GameScore.BAD: {
+        return '312244';
+      }
+    }
+  };
+
+  const getMessageByScore = (score: GameScore): string => {
+    switch (score) {
+      case GameScore.GOOD: {
+        return 'Congratulations! You are the best!';
+      }
+      case GameScore.MEDIOCRE: {
+        return 'Good job but probably you could do better!';
+      }
+      case GameScore.BAD: {
+        return 'Maybe try again and get a better score?';
+      }
+    }
+  };
 
   return (
     <div className="GameResult">
-      <FontAwesomeIcon icon={faTrophy} size={'6x'} style={{color: color}}/>
+      <FontAwesomeIcon icon={faTrophy} size={'6x'} style={{color: getColorByScore(gameScore)}}/>
       <p className="GameResult__text">
-        Congratulations! You finished in {moves} moves.
+        {getMessageByScore(gameScore)}
+        <br/>
+        You finished in {moves} moves.
       </p>
     </div>
   );
