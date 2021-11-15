24b7fd4012966e78fe1358c408b584a75a3001a7
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-11-15 20:52:47 +0100

    Move mazeSize constant to file where it is used

--- src/hooks/useMaze.ts
+++ src/hooks/useMaze.ts
@@ -1,7 +1,13 @@
 import { useCallback, useState } from 'react';
 import { generateMaze } from '@/helpers/maze';
 import { Difficulty } from '@/model/enums/difficulty';
-import { MAZE_SIZE } from '@/constants/mazeSize';
+import { Size } from '@/model/size';
+
+const MAZE_SIZE: Record<Difficulty, Size> = {
+  [Difficulty.EASY]: { width: 15, height: 15 },
+  [Difficulty.NORMAL]: { width: 20, height: 20 },
+  [Difficulty.HARD]: { width: 25, height: 25 },
+};
 
 export const useMaze = (initialDifficulty: Difficulty) => {
   const [maze, setMaze] = useState(() =>
